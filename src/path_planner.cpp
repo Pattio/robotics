876131f2aed489502b2b2bfa3a1254e2753772cc
Pattio <merfisku@gmail.com>
2017-11-14 15:24:06 +0000

    Use heuristic for sorting points, cleanup path planner

--- src/path_planner.cpp
+++ src/path_planner.cpp
@@ -3,20 +3,47 @@
 #include <geometry_msgs/PoseArray.h>
 #include <AStar.hpp>
 #include <marker.hpp>
+#include <geometry.hpp>
 #include <thread>
+
 
 Marker *marker;
 AStar::Map *myMap;
 AStar::AStarAlgorithm *algorithm;
-ros::Publisher move_publisher;
-ros::Publisher map_publisher;
+ros::Publisher move_publisher, map_publisher;
 
 std::vector<std::vector<double>> goals;
-std::vector<AStar::Position> paths[5];
-std::vector<AStar::Position> waypoints[5];
+std::vector<AStar::Position> paths[5], waypoints[5];
 
-std::vector<AStar::Position> extractWaypoints(std::vector<AStar::Position> path);
+void drawMarkersForPath(const std::vector<AStar::Position> &path);
+void drawWaypointsForPath(const std::vector<AStar::Position> &path);
+void findPath(AStar::Position start, AStar::Position end, int pathIndex);
+void makePathOptimal();
+void readParameters(ros::NodeHandle nh);
+void readOccupancyGrid(const nav_msgs::OccupancyGrid msg);
 void publishWaypoints();
+std::vector<AStar::Position> extractWaypoints(std::vector<AStar::Position> path);
+
+int main(int argc, char **argv) {
+	// Initialize ROS and become ROS node
+	ros::init(argc, argv, "path_planner");
+	ros::NodeHandle nh;
+	// Create marker object
+	marker = new Marker(nh, "path", "map");
+	// Create publisher which publishes waypoints to driver
+	move_publisher = nh.advertise<geometry_msgs::PoseArray>("/drive_waypoints", 1);
+	map_publisher = nh.advertise<nav_msgs::OccupancyGrid>("/map", 1);
+	// Read parameters
+	readParameters(nh);
+
+	ros::Subscriber sub = nh.subscribe("map", 1000, &readOccupancyGrid);
+	ros::spin();
+	
+	// Clean memory
+	if(algorithm != nullptr) delete algorithm;
+	if(myMap != nullptr) delete myMap;
+	if(marker != nullptr) delete marker;
+}
 
 void drawMarkersForPath(const std::vector<AStar::Position> &path) {
 	geometry_msgs::Pose pose;
@@ -45,35 +72,69 @@ void findPath(AStar::Position start, AStar::Position end, int pathIndex) {
 	drawMarkersForPath(paths[pathIndex]);
 }
 
-// TODO: REVERT BACK AFTER TESTING const nav_msgs::OccupancyGrid::ConstPtr& msg
-void readOccupancyGrid(const nav_msgs::OccupancyGrid msg) {
+// Using euclidean heuristic compare each point to sort points by 
+// increasing order
+void makePathOptimal() {
+	std::vector<std::vector<double>> goalsCopy = goals;
+	std::vector<std::vector<double>> heuristicGoals;
+	heuristicGoals.push_back(goals.at(0));
+	std::vector<double> currentPoint = goals.at(0);
+	goalsCopy.erase (goalsCopy.begin());
 
+	double minDistanceIndex = 0, heuristicDistance = 0;
+	double minDistance = std::numeric_limits<double>::max();
+	
+	for(int j = 0; j < goals.size() - 1; j++) {
+		for(int i = 0; i < goalsCopy.size(); i++) {
+			heuristicDistance = sqrt(
+				pow((goalsCopy.at(i).at(0) - currentPoint.at(0)), 2) + 
+				pow((goalsCopy.at(i).at(1) - currentPoint.at(1)), 2)
+			);
+			if(heuristicDistance < minDistance) {
+				minDistanceIndex = i;
+				minDistance = heuristicDistance;
+			}
+		}
+		currentPoint = goalsCopy.at(minDistanceIndex);
+		heuristicGoals.push_back(currentPoint);
+		goalsCopy.erase(goalsCopy.begin() + minDistanceIndex);
+		minDistance = std::numeric_limits<double>::max();
+	}
+	goals = heuristicGoals;
+}
+
+void readParameters(ros::NodeHandle nh) {
+	// Prepare vector for 6 goals
+	goals.resize(6);
+	// Get robot start position
+	nh.getParam("robot_start", goals.at(0));
+	// Get goal points
+	for(int i = 1; i < 6; i++) {
+		nh.getParam("goal" + std::to_string(i - 1), goals.at(i));
+	}
+}
+
+void readOccupancyGrid(const nav_msgs::OccupancyGrid msg) {
     // Get map origin
     AStar::PositionMap mapOrigin(msg.info.origin.position.x, msg.info.origin.position.y);
     // Cast occupancy grid to int vector
     std::vector<int> grid(msg.data.begin(), msg.data.end());
     // Create map object with all the information
 	myMap = new AStar::Map(msg.info.height, msg.info.width, grid, 0, mapOrigin, msg.info.resolution);
-	// Inflate all obsticles by half of robot hypotenuse size (should be 14/1.2 = 11.7)
-	// myMap->inflateObsticles(8);
-	myMap->inflateObsticles(0.1, 0.1);
+	// Inflate all obsticles
+	myMap->inflateObsticles(robot_width, robot_height);
 	
-
+	// Publish inflated map
 	nav_msgs::OccupancyGrid gridCopy = msg;
-	// int p[] = {100, 100, 50, 0};
-	// std::vector<signed char> a(p, p+4);
-	// gridCopy.data = a;
 	gridCopy.data = myMap->getFlattenedMap();
 	map_publisher.publish(gridCopy);
 
+	// Make path optimal using euclidean heuristics
+	makePathOptimal();
 	
 	// Create 5 threads for each of the goals
 	std::thread threads[5];
 	algorithm = new AStar::AStarAlgorithm(*myMap);
-
-	// Make optimal path
-	std::iter_swap(goals.begin() + 3, goals.begin() + 4);
-	std::iter_swap(goals.begin() + 4, goals.begin() + 5);
 
 	// Find paths between each of the point pair
 	AStar::Position startPosition = myMap->transformMapPositionToGridPosition(AStar::PositionMap(goals.at(0).at(0), goals.at(0).at(1)));
@@ -90,78 +151,14 @@ void readOccupancyGrid(const nav_msgs::OccupancyGrid msg) {
 
 	// Publish waypoints to driver after path is planned
 	publishWaypoints();
-	
-	ROS_INFO("DONE");
+	ROS_INFO("DONE PLANNING PATH, LET'S START ROLLING. GOOD LUCK.");
 	exit(-1);
 }
 
-void readParameters(ros::NodeHandle nh) {
-	// Prepare vector for 6 goals
-	goals.resize(6);
-	// Get robot start position
-	nh.getParam("robot_start", goals.at(0));
-	// Get goal points
-	for(int i = 1; i < 6; i++) {
-		nh.getParam("goal" + std::to_string(i - 1), goals.at(i));
-	}
-}
-
-void publishWaypoints() {
-	geometry_msgs::PoseArray poseArray;
-	poseArray.header.stamp = ros::Time::now();
-  	poseArray.header.frame_id = "/map";
-	geometry_msgs::Pose pose;
-
-	double lastX, lastY, currentX, currentY;
-	for(int i = 0; i < 5; i++) {
-		for(int j = 0; j < waypoints[i].size(); j++) {
-			currentX = /*myMap->origin.x + */waypoints[i].at(j).x * myMap->cellResolution;
-			currentY = /*myMap->origin.y +*/ waypoints[i].at(j).y * myMap->cellResolution;
-			
-			// Skip duplicate of endpoint -> startpoint
-			if(lastX == currentX && lastY == currentY) continue;
-
-			pose.position.x = currentX;
-			pose.position.y = currentY;
-			poseArray.poses.push_back(pose);
-			lastX = currentX;
-			lastY = currentY;
-		}
-	}
-	move_publisher.publish(poseArray);
-}
-
-int main(int argc, char **argv) {
-
-	// Initialize ROS and become ROS node
-	ros::init(argc, argv, "path_planner");
-	ros::NodeHandle nh;
-	// Create marker object
-	marker = new Marker(nh, "path", "map");
-	// Create publisher which publishes waypoints to driver
-	move_publisher = nh.advertise<geometry_msgs::PoseArray>("/drive_waypoints", 1);
-	map_publisher = nh.advertise<nav_msgs::OccupancyGrid>("/map", 1);
-	// Read parameters
-	readParameters(nh);
-
-	// Use wait for message (practical)
-	// rospy.wait_for_message(’/map’,OccupancyGrid,timeout=None)
-	ros::Subscriber sub = nh.subscribe("map", 1000, &readOccupancyGrid);
-	ros::spin();
-	
-	// Clean memory
-	if(algorithm != nullptr) delete algorithm;
-	if(myMap != nullptr) delete myMap;
-	if(marker != nullptr) delete marker;
-}
-
-
 std::vector<AStar::Position> extractWaypoints(std::vector<AStar::Position> path) {
     std::vector<AStar::Position> waypoints;
-    
-    // TODO: CHANGE FROM HARDCODED VALUE TO PARAMETER
-    // Currently set to 4 as it is half of the robot size
-    int minWaypointDistance = 0;
+ 	// How accurate waypoints you need, currently set to the robot size
+    int minWaypointDistance = robot_height / myMap->cellResolution;
     int currentWaypointDistance = 0;
     
     // Impossible values, so first point will be pushed
@@ -191,4 +188,29 @@ std::vector<AStar::Position> extractWaypoints(std::vector<AStar::Position> path)
     // Push last point of the path
     if(!path.empty()) waypoints.push_back(path.back());
     return waypoints;
+}
+
+void publishWaypoints() {
+	geometry_msgs::PoseArray poseArray;
+	poseArray.header.stamp = ros::Time::now();
+  	poseArray.header.frame_id = "/map";
+	geometry_msgs::Pose pose;
+
+	double lastX, lastY, currentX, currentY;
+	for(int i = 0; i < 5; i++) {
+		for(int j = 0; j < waypoints[i].size(); j++) {
+			currentX = waypoints[i].at(j).x * myMap->cellResolution;
+			currentY = waypoints[i].at(j).y * myMap->cellResolution;
+			
+			// Skip duplicate of endpoint -> startpoint
+			if(lastX == currentX && lastY == currentY) continue;
+
+			pose.position.x = currentX;
+			pose.position.y = currentY;
+			poseArray.poses.push_back(pose);
+			lastX = currentX;
+			lastY = currentY;
+		}
+	}
+	move_publisher.publish(poseArray);
 }
